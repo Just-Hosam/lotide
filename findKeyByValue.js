daecb202660a02ff72ddc98e2853b4fdec80d345
Hosam  Dahrooge <Hosam_firas@hotmail.com>
2021-02-03 15:01:22 -0700

    added logic to function

--- findKeyByValue.js
+++ findKeyByValue.js
@@ -1,4 +1,3 @@
-// FUNCTION IMPLEMENTATION
 const assertEqual = function(actual, expected) {
 
   if (actual === expected) {
@@ -8,3 +7,25 @@ const assertEqual = function(actual, expected) {
   }
   
 };
+
+const findKeyByValue = (inputObj, inputStr) => {
+  let keyArr = Object.keys(inputObj);
+
+  for (const element of keyArr) {
+    if (inputObj[element] === inputStr) {
+      return element;
+    }
+  }
+
+};
+
+const bestTVShowsByGenre = { 
+  sci_fi: "The Expanse",
+  comedy: "Brooklyn Nine-Nine",
+  drama:  "The Wire",
+  anime: 'Koe No Katachi'
+};
+
+assertEqual(findKeyByValue(bestTVShowsByGenre, "The Wire"), "drama");
+assertEqual(findKeyByValue(bestTVShowsByGenre, "That '70s Show"), undefined);
+assertEqual(findKeyByValue(bestTVShowsByGenre, "Koe No Katachi"), 'anime');
