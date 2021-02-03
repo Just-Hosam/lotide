7a83af4c17595a550c6b15a4a79de8896c0d88ce
Hosam  Dahrooge <Hosam_firas@hotmail.com>
2021-02-03 14:20:08 -0700

    Added logic to countLetters

--- countLetters.js
+++ countLetters.js
@@ -7,3 +7,29 @@ const assertEqual = function(actual, expected) {
   }
   
 };
+
+const countLetters = (inputStr) => {
+  let letterCount = {};
+
+  for (const letter of inputStr) {
+    if (letter === ' ') {
+      continue;
+    }
+    (letterCount[letter]) ? letterCount[letter]++ : letterCount[letter] = 1;
+  }
+
+  return letterCount;
+}
+
+const testObj = countLetters("lighthouse in the house");
+
+assertEqual(testObj.l, 1);
+assertEqual(testObj.i, 2);
+assertEqual(testObj.g, 1);
+assertEqual(testObj.h, 4);
+assertEqual(testObj.t, 2);
+assertEqual(testObj.o, 2);
+assertEqual(testObj.u, 2);
+assertEqual(testObj.s, 2);
+assertEqual(testObj.e, 3);
+assertEqual(testObj.n, 1);
