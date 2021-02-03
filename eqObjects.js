666816a38f24c0873c2c14ee16705a5e23c857a4
Hosam  Dahrooge <Hosam_firas@hotmail.com>
2021-02-03 15:48:39 -0700

    created logic fir function (given steps are bad)

--- eqObjects.js
+++ eqObjects.js
@@ -8,13 +8,32 @@ const assertEqual = function(actual, expected) {
   
 };
 
+const eqArrays = (actualArr, expectedArr) => {
+
+  for (let i = 0; i < actualArr.length; i++) {
+    if (actualArr[i] !== expectedArr[i] || (actualArr.length !== expectedArr.length)) {
+      return false;
+    }
+  }
+
+  return true;
+}
+
 const eqObjects = function(object1, object2) {
   let sortedFirstKeys = Object.keys(object1).sort();
   let sortedSecondKeys = Object.keys(object2).sort();
 
   if (sortedFirstKeys.length !== sortedSecondKeys.length) { return false; };
+  if (!eqArrays(sortedFirstKeys, sortedSecondKeys)) { return false; };
+
   for (let i = 0; i < sortedFirstKeys.length; i++) {
-    if (object1[sortedFirstKeys[i]] !== object2[sortedSecondKeys[i]]) { return false; }
+
+    if (Array.isArray(object1[sortedFirstKeys[i]]) && Array.isArray(object1[sortedSecondKeys[i]])) {
+      if (!eqArrays(object1[sortedFirstKeys[i]], object2[sortedSecondKeys[i]])) { return false; };
+    } else if (object1[sortedFirstKeys[i]] !== object2[sortedSecondKeys[i]]) { 
+      return false; 
+    }
+
   }
 
   return true;
