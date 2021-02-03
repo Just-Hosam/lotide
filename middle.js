042f15ac7b6f9645da524fb3d04ea0b09743b08f
Hosam  Dahrooge <Hosam_firas@hotmail.com>
2021-02-03 12:52:51 -0700

    Added test cases

--- middle.js
+++ middle.js
@@ -29,3 +29,12 @@ const middle = (inputArr) => {
 
   return finalArr;
 }
+
+assertArraysEqual(middle([]), []);
+assertArraysEqual(middle([1]), []);
+assertArraysEqual(middle([1, 2]), []);
+assertArraysEqual(middle([1, 2, 3]), [2]);
+assertArraysEqual(middle([1, 2, 3, 4]), [2, 3]);
+assertArraysEqual(middle([1, 2, 3, 4, 5]), [3]);
+assertArraysEqual(middle([1, 2, 3, 4, 5, 6]), [3, 4]);
+assertArraysEqual(middle([1, 2, 3, 4, 5, 6, 7]), [4]);
