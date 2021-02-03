85bcc7c1771a203105f0b13315cf692460c97f31
Hosam  Dahrooge <Hosam_firas@hotmail.com>
2021-02-03 12:50:11 -0700

    Added middle function

--- middle.js
+++ middle.js
@@ -18,3 +18,14 @@ const assertArraysEqual = (actualArr, expectedArr) => {
   }
   
 }
+
+const middle = (inputArr) => {
+  let centerOfArr = inputArr.length / 2;
+  let finalArr = [];
+  
+  inputArr.length % 2 === 0 ? finalArr = [inputArr[centerOfArr - 1], inputArr[centerOfArr]] : null;
+  inputArr.length % 2 !== 0 ? finalArr = [inputArr[Math.floor(centerOfArr)]] : null;
+  inputArr.length <= 2 ? finalArr = [] : null;
+
+  return finalArr;
+}
