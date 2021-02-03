9c28f6da4aa5f706f1bf72f7a7268e052c0ee530
Hosam  Dahrooge <Hosam_firas@hotmail.com>
2021-02-03 14:29:21 -0700

    Added assertions

---
+++ letterPositions.js
@@ -0,0 +1,20 @@
+const eqArrays = (actualArr, expectedArr) => {
+
+  for (let i = 0; i < actualArr.length; i++) {
+    if (actualArr[i] !== expectedArr[i] || (actualArr.length !== expectedArr.length)) {
+      return false;
+    }
+  }
+
+  return true;
+}
+
+const assertArraysEqual = (actualArr, expectedArr) => {
+
+  if (eqArrays(actualArr, expectedArr)) {
+    console.log(`✅✅✅ Assertion Passed: ${actualArr} === ${expectedArr}`);
+  } else {
+    console.log(`❌❌❌ Assertion Failed: ${actualArr} !== ${expectedArr}`);
+  }
+  
+}
