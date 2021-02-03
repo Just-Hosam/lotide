d216f63c93640b7ac87669c2bc3f36be582cf347
Hosam  Dahrooge <Hosam_firas@hotmail.com>
2021-02-03 15:26:01 -0700

    implemented logic to compare primitive values

--- eqObjects.js
+++ eqObjects.js
@@ -7,3 +7,23 @@ const assertEqual = function(actual, expected) {
   }
   
 };
+
+const eqObjects = function(object1, object2) {
+  let sortedFirstKeys = Object.keys(object1).sort();
+  let sortedSecondKeys = Object.keys(object2).sort();
+
+  if (sortedFirstKeys.length !== sortedSecondKeys.length) { return false; };
+  for (let i = 0; i < sortedFirstKeys.length; i++) {
+    if (object1[sortedFirstKeys[i]] !== object2[sortedSecondKeys[i]]) { return false; }
+  }
+
+  return true;
+};
+
+const cd = { c: "1", d: ["2", 3] };
+const dc = { d: ["2", 3], c: "1" };
+assertEqual(eqObjects(cd, dc), true); // => true
+
+const cd2 = { c: "1", d: ["2", 3, 4] };
+assertEqual(eqObjects(cd, cd2), false); // => false
+
