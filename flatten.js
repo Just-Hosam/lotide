50f8960925f6dec6a83d0e38d7968785526c6588
Hosam  Dahrooge <Hosam_firas@hotmail.com>
2021-02-02 18:34:49 -0700

    created function that flatten the nested arrays

--- flatten.js
+++ flatten.js
@@ -21,3 +21,35 @@ const assertArraysEqual = (actualArr, expectedArr) => {
   }
   
 }
+
+const flatten = nestArrays => {
+  let flatArr = [];
+
+  for (const element of nestArrays) {
+    if (element.length >= 1) {
+      for (let k = 0; k < element.length; k++) {
+        flatArr.push(element[k]);
+      }
+    } else if (element.length === 0){
+      continue;
+    } else {
+      flatArr.push(element);
+    }
+  } 
+
+  return flatArr;
+}
+
+console.log(flatten([1, 2, [3, 4], 5, [6], [], ['lol', 7]]));
+
+
+
+
+
+
+
+
+
+
+
+
