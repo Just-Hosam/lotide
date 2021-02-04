54babfeea5b756a73b3bfe4f019695e768f13e53
Hosam  Dahrooge <Hosam_firas@hotmail.com>
2021-02-04 15:05:30 -0700

    full commit

--- findKey.js
+++ findKey.js
@@ -7,3 +7,24 @@ const assertEqual = function(actual, expected) {
   }
   
 };
+
+const findKey = (inputObj, callback) => {
+  let keyArr = Object.keys(inputObj);
+
+  for (const elem of keyArr) {
+    if (callback(inputObj[elem])) { return elem; };
+  }
+}
+
+const dataObj1 = {
+  "Blue Hill": { stars: 1 },
+  "Akaleri":   { stars: 3 },
+  "noma":      { stars: 2 },
+  "elBulli":   { stars: 3 },
+  "Ora":       { stars: 2 },
+  "Akelarre":  { stars: 3 }
+};
+
+assertEqual(findKey(dataObj1, x => x.stars === 2), 'noma');
+assertEqual(findKey(dataObj1, x => x.stars > 5), undefined);
+assertEqual(findKey(dataObj1, x => x.stars >= 2), 'Akaleri');
