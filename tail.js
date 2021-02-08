f8b91fa46d39d3e6c1b4a5fe462384acc0bfa48e
Hosam  Dahrooge <Hosam_firas@hotmail.com>
2021-02-08 09:22:41 -0700

    refactored and exported the file

--- tail.js
+++ tail.js
@@ -1,11 +1,3 @@
-const assertEqual = function(actual, expected) {
-
-  if (actual === expected) {
-    console.log(`✅✅✅ Assertion Passed: ${actual} === ${expected}`);
-  } else {
-    console.log(`❌❌❌ Assertion Failed: ${actual} !== ${expected}`);
-  }
-  
-};
-
 const tail = arr => arr.slice(1);
+
+module.exports = tail;
