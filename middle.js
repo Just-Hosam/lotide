8a1b6537e09f3a5a396a712f8694a29cae8672dd
Hosam  Dahrooge <Hosam_firas@hotmail.com>
2021-02-08 09:26:49 -0700

    refactored the file and exported it

--- middle.js
+++ middle.js
@@ -1,24 +1,3 @@
-const eqArrays = (actualArr, expectedArr) => {
-
-  for (let i = 0; i < actualArr.length; i++) {
-    if (actualArr[i] !== expectedArr[i] || (actualArr.length !== expectedArr.length)) {
-      return false;
-    }
-  }
-
-  return true;
-}
-
-const assertArraysEqual = (actualArr, expectedArr) => {
-
-  if (eqArrays(actualArr, expectedArr)) {
-    console.log(`✅✅✅ Assertion Passed: ${actualArr} === ${expectedArr}`);
-  } else {
-    console.log(`❌❌❌ Assertion Failed: ${actualArr} !== ${expectedArr}`);
-  }
-  
-}
-
 const middle = (inputArr) => {
   let centerOfArr = inputArr.length / 2;
   let finalArr = [];
@@ -30,11 +9,4 @@ const middle = (inputArr) => {
   return finalArr;
 }
 
-assertArraysEqual(middle([]), []);
-assertArraysEqual(middle([1]), []);
-assertArraysEqual(middle([1, 2]), []);
-assertArraysEqual(middle([1, 2, 3]), [2]);
-assertArraysEqual(middle([1, 2, 3, 4]), [2, 3]);
-assertArraysEqual(middle([1, 2, 3, 4, 5]), [3]);
-assertArraysEqual(middle([1, 2, 3, 4, 5, 6]), [3, 4]);
-assertArraysEqual(middle([1, 2, 3, 4, 5, 6, 7]), [4]);
+module.exports = middle;
