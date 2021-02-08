806fa9f9ec4e170c0d8b69b6c049c6111bd8cbf9
Hosam  Dahrooge <Hosam_firas@hotmail.com>
2021-02-08 12:05:21 -0700

    refactored assertions using mocha and chai

--- test/middleTest.js
+++ test/middleTest.js
@@ -1,12 +1,37 @@
-const assertArraysEqual = require('../assertArraysEqual');
+const assert = require('chai').assert;
 
 const middle = require('../middle');
 
-assertArraysEqual(middle([]), []);
-assertArraysEqual(middle([1]), []);
-assertArraysEqual(middle([1, 2]), []);
-assertArraysEqual(middle([1, 2, 3]), [2]);
-assertArraysEqual(middle([1, 2, 3, 4]), [2, 3]);
-assertArraysEqual(middle([1, 2, 3, 4, 5]), [3]);
-assertArraysEqual(middle([1, 2, 3, 4, 5, 6]), [3, 4]);
-assertArraysEqual(middle([1, 2, 3, 4, 5, 6, 7]), [4]);
+describe('Tests for middle.js', () => {
+  it('should return [] when given []', () => {
+    assert.deepEqual(middle([]), []);
+  });
+
+  it('should return [] when given [1]', () => {
+    assert.deepEqual(middle([1]), []);
+  });
+
+  it('should return [] when given [1, 2]', () => {
+    assert.deepEqual(middle([1, 2]), []);
+  });
+
+  it('should return [2] when given [1, 2, 3]', () => {
+    assert.deepEqual(middle([1, 2, 3]), [2]);
+  });
+
+  it('should return [2, 3] when given [1, 2, 3, 4]', () => {
+    assert.deepEqual(middle([1, 2, 3, 4]), [2, 3]);
+  });
+
+  it('should return [3] when given [1, 2, 3, 4, 5]', () => {
+    assert.deepEqual(middle([1, 2, 3, 4, 5]), [3]);
+  });
+
+  it('should return [3, 4] when given [1, 2, 3, 4, 5, 6]', () => {
+    assert.deepEqual(middle([1, 2, 3, 4, 5, 6]), [3, 4]);
+  });
+
+  it('should return [4] when given [1, 2, 3, 4, 5, 6, 7]', () => {
+    assert.deepEqual(middle([1, 2, 3, 4, 5, 6, 7]), [4]);
+  });
+});
