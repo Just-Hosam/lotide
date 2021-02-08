bf240a2931072fb3dc808f48b12c396b6ca02e50
Hosam  Dahrooge <Hosam_firas@hotmail.com>
2021-02-08 13:43:58 -0700

    refactored eqObjecs

--- eqObjects-Recursive.js
+++ eqObjects-Recursive.js
@@ -1,26 +1,5 @@
 const eqArrays = require('./eqArrays-Recursive');
 
-const assertEqual = function(actual, expected) {
-
-  if (actual === expected) {
-    console.log(`✅✅✅ Assertion Passed: ${actual} === ${expected}`);
-  } else {
-    console.log(`❌❌❌ Assertion Failed: ${actual} !== ${expected}`);
-  }
-  
-};
-
-// const eqArrays = (actualArr, expectedArr) => {
-
-//   for (let i = 0; i < actualArr.length; i++) {
-//     if (actualArr[i] !== expectedArr[i] || (actualArr.length !== expectedArr.length)) {
-//       return false;
-//     }
-//   }
-
-//   return true;
-// }
-
 const eqObjects = function(object1, object2) {
   let sortedFirstKeys = Object.keys(object1).sort();
   let sortedSecondKeys = Object.keys(object2).sort();
@@ -44,15 +23,3 @@ const eqObjects = function(object1, object2) {
 };
 
 module.exports = eqObjects;
-
-const cd = { c: "1", d: ["2", 3] };
-const dc = { d: ["2", 3], c: "1" };
-assertEqual(eqObjects(cd, dc), true);
-
-const cd2 = { c: "1", d: ["2", 3, 4] };
-assertEqual(eqObjects(cd, cd2), false);
-
-assertEqual(eqObjects({ a: { z: 1 }, b: 2 }, { a: { z: 1 }, b: 2 }), true);
-assertEqual(eqObjects({ a: { y: 0, z: 1 }, b: 2 }, { a: { z: 1 }, b: 2 }), false);
-assertEqual(eqObjects({ a: { y: 0, z: 1 }, b: 2 }, { a: 1, b: 2 }), false);
-
