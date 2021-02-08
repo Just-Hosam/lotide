e8800c3febef2a1368095feb6f0f7f84acf066d3
Hosam  Dahrooge <Hosam_firas@hotmail.com>
2021-02-08 10:52:01 -0700

    refactored with mocha and chai

--- test/headTest.js
+++ test/headTest.js
@@ -1,6 +1,13 @@
-const assertEqual = require('../assertEqual.js');
+const assert = require('chai').assert;
 
 const head = require('../head.js');
 
-assertEqual(head([1, 2, 3, 4]), 1);
-assertEqual(head(['test1', 'test2', 'test3', 'test4']), 'test1');
+describe('headTest', () => {
+  it('should return 1 for [1, 2, 3, 4]', () =>{
+    assert.strictEqual(head([1, 2, 3, 4]), 1);
+  });
+
+  it('should return "test1" for ["test1", "test2", "test3", "test4"]', () => {
+    assert.strictEqual(head(['test1', 'test2', 'test3', 'test4']), 'test1');
+  });
+});
