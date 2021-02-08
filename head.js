52438132614b02c3d8bdc7270c1ee44260ad9627
Hosam  Dahrooge <Hosam_firas@hotmail.com>
2021-02-08 09:00:47 -0700

    added require statement and assert tests

--- head.js
+++ head.js
@@ -1,11 +1,6 @@
-const assertEqual = function(actual, expected) {
-
-  if (actual === expected) {
-    console.log(`✅✅✅ Assertion Passed: ${actual} === ${expected}`);
-  } else {
-    console.log(`❌❌❌ Assertion Failed: ${actual} !== ${expected}`);
-  }
-  
-};
+const assertEqual = require('./assertEqual.js');
 
 const head = arr => arr[0];
+
+assertEqual(head([1, 2, 3, 4]), 1);
+assertEqual(head(['test1', 'test2', 'test3', 'test4']), 'test1');
