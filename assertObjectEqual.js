5d26212db788509e01610060a10b2702ab031ee7
Hosam  Dahrooge <Hosam_firas@hotmail.com>
2021-02-08 13:53:56 -0700

    refactored

--- assertObjectEqual.js
+++ assertObjectEqual.js
@@ -1,33 +1,5 @@
-const eqArrays = (actualArr, expectedArr) => {
-
-  for (let i = 0; i < actualArr.length; i++) {
-    if (actualArr[i] !== expectedArr[i] || (actualArr.length !== expectedArr.length)) {
-      return false;
-    }
-  }
-
-  return true;
-};
-
-const eqObjects = function(object1, object2) {
-  let sortedFirstKeys = Object.keys(object1).sort();
-  let sortedSecondKeys = Object.keys(object2).sort();
-
-  if (sortedFirstKeys.length !== sortedSecondKeys.length) { return false; };
-  if (!eqArrays(sortedFirstKeys, sortedSecondKeys)) { return false; };
-
-  for (let i = 0; i < sortedFirstKeys.length; i++) {
-
-    if (Array.isArray(object1[sortedFirstKeys[i]]) && Array.isArray(object1[sortedSecondKeys[i]])) {
-      if (!eqArrays(object1[sortedFirstKeys[i]], object2[sortedSecondKeys[i]])) { return false; };
-    } else if (object1[sortedFirstKeys[i]] !== object2[sortedSecondKeys[i]]) { 
-      return false; 
-    }
-
-  }
-
-  return true;
-};
+const { assert } = require('console');
+const eqObjects = require('./eqObjects-Recursive');
 
 const assertObjectEqual = (actualObj, expectedObj) => {
   const inspect = require('util').inspect;
@@ -39,6 +11,8 @@ const assertObjectEqual = (actualObj, expectedObj) => {
   }
 
 }
+
+module.exports = assertObjectEqual;
 
 const cd = { c: "1", d: ["2", 3] };
 const dc = { d: ["2", 3], c: "1" };
