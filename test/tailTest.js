95bdc1e5a5eb5fca45754d0c1f62a63fd9a09900
Hosam  Dahrooge <Hosam_firas@hotmail.com>
2021-02-08 11:57:09 -0700

    cleaned up the comments

--- test/tailTest.js
+++ test/tailTest.js
@@ -1,9 +1,6 @@
-// const assertArraysEqual = require('../assertArraysEqual');
 const assert = require('chai').assert;
 
 const tail = require('../tail.js');
-
-// assertArraysEqual(tail([1, 2, 3]), [2, 3]);
 
 describe('Tests for tail.js', () => {
   it('should return [2, 3] for [1, 2, 3]', () => {
