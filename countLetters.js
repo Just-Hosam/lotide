a8c88ee517c69fed8c90d2986efc6b9cd3a8fb50
Hosam  Dahrooge <Hosam_firas@hotmail.com>
2021-02-08 14:12:55 -0700

    refactored and exported

--- countLetters.js
+++ countLetters.js
@@ -1,13 +1,3 @@
-const assertEqual = function(actual, expected) {
-
-  if (actual === expected) {
-    console.log(`✅✅✅ Assertion Passed: ${actual} === ${expected}`);
-  } else {
-    console.log(`❌❌❌ Assertion Failed: ${actual} !== ${expected}`);
-  }
-  
-};
-
 const countLetters = (inputStr) => {
   let letterCount = {};
 
@@ -21,15 +11,4 @@ const countLetters = (inputStr) => {
   return letterCount;
 }
 
-const testObj = countLetters("lighthouse in the house");
-
-assertEqual(testObj.l, 1);
-assertEqual(testObj.i, 2);
-assertEqual(testObj.g, 1);
-assertEqual(testObj.h, 4);
-assertEqual(testObj.t, 2);
-assertEqual(testObj.o, 2);
-assertEqual(testObj.u, 2);
-assertEqual(testObj.s, 2);
-assertEqual(testObj.e, 3);
-assertEqual(testObj.n, 1);
+module.exports = countLetters;
